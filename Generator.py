70d574aa363532a6241c6416749d38a381a059ad
pierre Brault <pierrebrault@MacBook-Pro-de-pierre.local>
2024-11-27 11:10:11 -0400

    init

---
+++ Generator.py
@@ -0,0 +1,87 @@
+from typing import Union, List
+import os
+import time
+
+from PIL import Image
+from numpy.random import randint
+from diffusers import StableDiffusionPipeline
+import torch
+
+class Generator:
+    def __init__(self,
+                 prompts: Union[List[str], str],
+                 model_name: [str],
+                 neg_prompts: [Union[List[str], str]],
+                 steps: [int],
+                 height: [int],
+                 width: [int],
+                 seed: int,
+                 cfg: [float],
+                 output_dir: [str],
+                 num_images_per_prompt: int,
+                 random_seed_after_every_gen: [bool],
+                 device: str
+                 ):
+        self.prompts = prompts
+
+        self.neg_prompts = neg_prompts
+
+        self.model_name = model_name
+
+        self.steps = steps
+        self.height = height
+        self.width = width
+        self.num_images_per_prompt = num_images_per_prompt
+
+        if not seed:
+            if random_seed_after_every_gen:
+                seed = randint(2 ** 32)
+            else:
+                seed = 0
+
+        self.seed = seed
+        self.cfg = cfg
+        self.output_dir = output_dir
+
+        if not device:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+        elif device not in ["cuda", "cpu"]:
+            raise ValueError(f"device must be either 'cuda' or 'cpu', not {device}")
+
+        if not output_dir:
+            output_dir = "output_images"
+        self.output_dir = output_dir
+
+        self.device = device
+
+        self.pipe: StableDiffusionPipeline = None
+        self.images: List[Image] = []
+
+
+    def load_model(self):
+        self.pipe = StableDiffusionPipeline.from_pretrained(self.model_name).to(self.device)
+
+    def predict(self):
+        with torch.no_grad():
+            self.images = self.pipe(
+                prompt = self.prompts,
+                height = self.height,
+                width = self.width,
+                num_inference_steps = self.steps,
+                guidance_scale = self.cfg,
+                negative_prompt = self.neg_prompts,
+                num_images_per_prompt = self.num_images_per_prompt,
+                generator = torch.Generator(device=self.device).manual_seed(self.seed),
+            ).images
+
+    def save_images(self):
+        os.makedirs(self.output_dir, exist_ok=True)
+
+        for img in self.images:
+            full_path = os.path.join(self.output_dir, f"{str(time.time())}.png")
+            img.save(full_path)
+
+    def run(self):
+        self.load_model()
+        self.predict()
+        self.save_images()
---
+++ comfy2py.py
@@ -0,0 +1,40 @@
+from typing import Union, List, Tuple, Optional
+
+import fire
+
+from Generator import Generator
+
+def run(prompt: Union[List[str], str],
+        model_name: Optional[str] = "runwayml/stable-diffusion-v1-5",
+        neg_prompt: Optional[Union[List[str], str]] = "",
+        steps: Optional[int] = 50,
+        height: Optional[int] = 1024,
+        width: Optional[int] = 1024,
+        seed: int = None,
+        cfg: Optional[float] = 8,
+        output_dir: Optional[str] = "",
+        num_images_per_prompt: Optional[int] = 1,
+        random_seed_after_every_gen: Optional[bool] = True,
+        device: Optional[str] = None):
+
+    generator = Generator(
+        prompts = prompt,
+        model_name = model_name,
+        neg_prompts = neg_prompt,
+        steps = steps,
+        height = height,
+        width = width,
+        seed = seed,
+        cfg = cfg,
+        output_dir = output_dir,
+        num_images_per_prompt = num_images_per_prompt,
+        random_seed_after_every_gen = random_seed_after_every_gen,
+        device = device
+    )
+
+    generator.load_model()
+    generator.predict()
+    generator.save_images()
+
+if __name__ == "__main__":
+    fire.Fire(run)
