8e563ba60d0630d5fad68e7feeb664f6533a3db9
pierre Brault <pierrebrault@MacBook-Pro-de-pierre.local>
2024-11-27 15:28:58 -0400

    update parser

--- comfy2py.py
+++ comfy2py.py
@@ -44,23 +44,22 @@ if __name__ == "__main__":
 
     # Définir les arguments attendus
     parser.add_argument("--prompt", type=str, required=True, help="The prompt describing the desired image(s).")
-    parser.add_argument("--model_name", type=str, default="runwayml/stable-diffusion-v1-5", help="Name of the model to use.")
-    parser.add_argument("--neg_prompt", type=str, default="", help="Negative prompt to specify elements to avoid.")
+    parser.add_argument("--model-name", type=str, default="runwayml/stable-diffusion-v1-5", help="Name of the model to use.")
+    parser.add_argument("--neg-prompt", type=str, default="", help="Negative prompt to specify elements to avoid.")
     parser.add_argument("--steps", type=int, default=50, help="Number of inference steps.")
     parser.add_argument("--height", type=int, default=512, help="Height of the generated image in pixels.")
     parser.add_argument("--width", type=int, default=512, help="Width of the generated image in pixels.")
     parser.add_argument("--seed", type=int, default=None, help="Seed for random generation.")
     parser.add_argument("--cfg", type=float, default=8.0, help="Classifier-Free Guidance scale.")
-    parser.add_argument("--output_dir", type=str, default="", help="Directory to save the generated images.")
-    parser.add_argument("--num_images_per_prompt", type=int, default=1, help="Number of images to generate per prompt.")
-    parser.add_argument("--random_seed_after_every_gen", type=bool, default=True, help="Randomize seed after every generation.")
-    parser.add_argument("--sampler_name", type=str, default="euler", help="Sampler to use.")
+    parser.add_argument("--output-dir", type=str, default="", help="Directory to save the generated images.")
+    parser.add_argument("--num-images-per-prompt", type=int, default=1, help="Number of images to generate per prompt.")
+    parser.add_argument("--random-seed_after-every-gen", type=bool, default=True, help="Randomize seed after every generation.")
+    parser.add_argument("--sampler-name", type=str, default="euler", help="Sampler to use.")
     parser.add_argument("--scheduler", type=str, default="normal", help="Scheduler to use.")
     parser.add_argument("--device", type=str, default=None, help="Device to use (e.g., 'cuda').")
 
     # Parse les arguments
     args = parser.parse_args()
-
     # Appel de la fonction `run` avec les arguments parsés
     run(
         prompt=args.prompt,
--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@ Ce repo permet de générer des images à partir de lignes de commande en utilis
 Vous pouvez générer une image en exécutant la commande suivante, le resultat sera stocker dans un dossier `output_images` :
 
 ```bash
-python comfy2py.py --prompt="a beautiful forest in autumn" --neg_prompt="no snow, no fog"
+python comfy2py.py --prompt="a beautiful forest in autumn" --neg-prompt="no snow, no fog"
 ```
 
 ## Arguments
@@ -26,7 +26,7 @@ python comfy2py.py --prompt="a beautiful forest in autumn" --neg_prompt="no snow
 - **Type** : Optional[int]
 - **Description** : Le nombre de step. Plus ce nombre est élevé, plus le processus de génération est long et détaillé. La valeur par défaut est 50.
 
-` --model_name`
+` --model-name`
 - **Type** : Optional[str]
 - **Description** : Le nom du modèle de diffusion à utiliser. La valeur par défaut est "runwayml/stable-diffusion-v1-5", mais vous pouvez utiliser un autre modèle compatible avec la class `DiffusionPipeline`.
 
@@ -46,19 +46,19 @@ python comfy2py.py --prompt="a beautiful forest in autumn" --neg_prompt="no snow
 - **Type** : Optional[float]
 - **Description** : Classifier-Free Guidance permet de contrôler la fidélité du modèle par rapport aux prompts. La valeur par défaut est 8.
 
-` --output_dir`
+` --output-dir`
 - **Type** : Optional[str]
 - **Description** : Le répertoire de sortie où l'image générée sera enregistrée. Par défaut, l'image est enregistrée dans `output_images`
 
-` --num_images_per_prompt`
+` --num-images-per-prompt`
 - **Type** : Optional[int]
 - **Description** : Le nombre d'images à générer pour chaque prompt. La valeur par défaut est 1.
 
-` --random_seed_after_every_gen`
+` --random-seed-after-every-gen`
 - **Type** : Optional[bool]
 - **Description** : Si activé, une nouvelle graine aléatoire sera générée après chaque image. `True` par default
 
-` --sampler_name`
+` --sampler-name`
 - **Type** : Optional[str]
 - **Description** : Le nom de l'échantillonneur à utiliser pour la génération. `euler` par default.
 
