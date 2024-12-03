6dedc8eeca56cb251554e32fa3e1be1cf5e736d5
pierre Brault <pierrebrault@MacBook-Pro-de-pierre.local>
2024-12-02 22:36:13 -0400

    add comfyUI workflow

--- comfy2py.py
+++ comfy2py.py
@@ -1,79 +1,46 @@
-from typing import Union, List, Optional
+import sys
+import os
+
+current_dir = os.path.dirname(__file__)
+utils_path = os.path.join(current_dir, "ComfyUI")
+sys.path.append(utils_path)
+
+import json
 
 import argparse
 
-from generator.generator import Generator
+from utils import pass_argument_to_workflow
+from ComfyUI.execution import PromptExecutor
+from ComfyUI.server import PromptServer
 
-def run(prompt: Union[List[str], str],
-        model_name: Optional[str] = "runwayml/stable-diffusion-v1-5",
-        neg_prompt: Optional[Union[List[str], str]] = "",
-        steps: Optional[int] = 50,
-        height: Optional[int] = 512,
-        width: Optional[int] = 512,
-        seed: int = None,
-        cfg: Optional[float] = 8,
-        output_dir: Optional[str] = "",
-        num_images_per_prompt: Optional[int] = 1,
-        random_seed_after_every_gen: Optional[bool] = True,
-        sampler_name: Optional[str] = "euler",
-        scheduler: Optional[str] = "normal",
-        device: Optional[str] = None):
-
-    generator = Generator(
-        prompts = prompt,
-        model_name = model_name,
-        neg_prompts = neg_prompt,
-        steps = steps,
-        height = height,
-        width = width,
-        seed = seed,
-        cfg = cfg,
-        output_dir = output_dir,
-        num_images_per_prompt = num_images_per_prompt,
-        random_seed_after_every_gen = random_seed_after_every_gen,
-        sampler_name = sampler_name,
-        scheduler = scheduler,
-        device = device
-    )
-
-    generator.run()
+def execute(workflow):
+    e = PromptExecutor(PromptServer(None))
+    e.execute(workflow, "", execute_outputs=["9"])
 
 if __name__ == "__main__":
     # Configuration d'argparse
-    parser = argparse.ArgumentParser(description="Run a stable diffusion image generator.")
+    parser = argparse.ArgumentParser(description="Run a ComfyUI workflow")
 
     # Définir les arguments attendus
-    parser.add_argument("--prompt", type=str, required=True, help="The prompt describing the desired image(s).")
-    parser.add_argument("--model-name", type=str, default="runwayml/stable-diffusion-v1-5", help="Name of the model to use.")
+    parser.add_argument("--prompt", type=str, default="test", help="The prompt describing the desired image(s).")
+    parser.add_argument("--workflow-path", type=str, default="workflows/default_workflow.json", required=False, help="Path to the workflow file.")
+    parser.add_argument("--model-name", type=str, default="v1-5-pruned-emaonly.safetensors", help="Name of the model to use.")
     parser.add_argument("--neg-prompt", type=str, default="", help="Negative prompt to specify elements to avoid.")
-    parser.add_argument("--steps", type=int, default=50, help="Number of inference steps.")
+    parser.add_argument("--steps", type=int, default=20, help="Number of inference steps.")
     parser.add_argument("--height", type=int, default=512, help="Height of the generated image in pixels.")
     parser.add_argument("--width", type=int, default=512, help="Width of the generated image in pixels.")
-    parser.add_argument("--seed", type=int, default=None, help="Seed for random generation.")
+    parser.add_argument("--seed", type=int, default=1, help="Seed for random generation.")
     parser.add_argument("--cfg", type=float, default=8.0, help="Classifier-Free Guidance scale.")
-    parser.add_argument("--output-dir", type=str, default="", help="Directory to save the generated images.")
+    parser.add_argument("--denois", type=float, default=1, help="Denois quantity")
+    parser.add_argument("--filename-prefix", type=str, default="ComfyUI", help="Directory to save the generated images.")
     parser.add_argument("--num-images-per-prompt", type=int, default=1, help="Number of images to generate per prompt.")
     parser.add_argument("--random-seed_after-every-gen", type=bool, default=True, help="Randomize seed after every generation.")
     parser.add_argument("--sampler-name", type=str, default="euler", help="Sampler to use.")
     parser.add_argument("--scheduler", type=str, default="normal", help="Scheduler to use.")
     parser.add_argument("--device", type=str, default=None, help="Device to use (e.g., 'cuda').")
 
-    # Parse les arguments
     args = parser.parse_args()
-    # Appel de la fonction `run` avec les arguments parsés
-    run(
-        prompt=args.prompt,
-        model_name=args.model_name,
-        neg_prompt=args.neg_prompt,
-        steps=args.steps,
-        height=args.height,
-        width=args.width,
-        seed=args.seed,
-        cfg=args.cfg,
-        output_dir=args.output_dir,
-        num_images_per_prompt=args.num_images_per_prompt,
-        random_seed_after_every_gen=args.random_seed_after_every_gen,
-        sampler_name=args.sampler_name,
-        scheduler=args.scheduler,
-        device=args.device
-    )
+    with open(args.workflow_path, 'r') as file:
+        workflow = json.load(file)
+    workflow = pass_argument_to_workflow(workflow, args)
+    execute(workflow)
---
+++ utils.py
@@ -0,0 +1,37 @@
+from typing import Dict, Union, List
+
+
+def get_nodes_from_class_type(workflow, class_type):
+    for key, value in workflow.items():
+        current_class_type = value.get('class_type')
+        if current_class_type == class_type:
+            return key
+    return None
+
+
+def pass_argument_to_workflow(workflow: Dict, args) -> Union[Dict, List[str]]:
+    sampler_node = get_nodes_from_class_type(workflow, "KSampler")
+    workflow[sampler_node]["inputs"]["seed"] = args.seed
+    workflow[sampler_node]["inputs"]["steps"] = args.steps
+    workflow[sampler_node]["inputs"]["cfg"] = args.cfg
+    workflow[sampler_node]["inputs"]["sampler_name"] = args.sampler_name
+    workflow[sampler_node]["inputs"]["scheduler"] = args.scheduler
+    workflow[sampler_node]["inputs"]["denois"] = args.denois
+
+    prompt_node = workflow[sampler_node]["inputs"]["positive"][0]
+    workflow[prompt_node]["inputs"]["text"] = args.prompt
+
+    neg_prompt_node = workflow[sampler_node]["inputs"]["negative"][0]
+    workflow[neg_prompt_node]["inputs"]["text"] = args.neg_prompt
+
+    model_node = get_nodes_from_class_type(workflow, "CheckpointLoaderSimple")
+    workflow[model_node]["inputs"]["ckpt_name"] = args.model_name
+
+    latent_node = get_nodes_from_class_type(workflow, "EmptyLatentImage")
+    workflow[latent_node]["inputs"]["height"] = args.height
+    workflow[latent_node]["inputs"]["width"] = args.width
+    workflow[latent_node]["inputs"]["batch_size"] = args.num_images_per_prompt
+
+    save_node = get_nodes_from_class_type(workflow, "SaveImage")
+    workflow[save_node]["inputs"]["filename_prefix"] = args.filename_prefix
+    return workflow
